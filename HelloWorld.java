232070a391381fe02fdb6fede6a9a5a73eb64e89
mailtovasu <39011613+mailtovasu@users.noreply.github.com>
2020-07-13 17:54:03 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld{
+public static void main(String b[]){
+
+System.out.println("Hello World!!!!!!!!!!");
+
+//Repository URL :     https://github.com/mailtovasu/hello-world.git
+
+}
+
+}
